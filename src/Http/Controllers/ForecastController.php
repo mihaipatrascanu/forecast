fd6938ddabaef68c4446db6bd65cfd2cd2332af5
Mihai Patrascanu <mihai@ecce.uk>
2022-10-20 11:53:57 +0300

    Refactory Forecast Controller

--- src/Http/Controllers/ForecastController.php
+++ src/Http/Controllers/ForecastController.php
@@ -11,6 +11,22 @@ use Mihai\Weather\Models\Day;
 
 class ForecastController extends Controller
 {
+     /**
+     * @var string
+     */
+    private $ip;
+
+    /**
+     * @var string
+     */
+    private $cityName;
+
+    public function __construct()
+    {
+        $this->get_ip_addreess();
+        $this->get_curent_location();
+    }
+
     public function get_ip_addreess()
     {
         $ip = '';
@@ -19,37 +35,54 @@ class ForecastController extends Controller
             {
                 $ip = request()->ip();
             }
-        return $ip;
+
+        $this->ip = $ip;
     }
 
-    public function get_curent_location($ip)
+    public function get_curent_location()
     {
-        $currentUserInfo = Location::get($ip);
-        return $currentUserInfo->cityName;
+        $this->cityName = Location::get($this->ip)->cityName;
     }
+
+    public function get_lat_long_by_ip()
+    {
+        return explode(",", file_get_contents('https://ipapi.co/' . $this->ip. '/latlong/'));
+    }
+
+    public function get_forecast_by_location()
+    {
     
-   
-
-    public function get_lat_long_by_ip($ip)
-    {
-        return explode(",", file_get_contents('https://ipapi.co/' . $ip . '/latlong/'));
-    }
-
-    public function get_forecast_by_location($city_name)
-    {
-        $weather = file_get_contents('https://api.openweathermap.org/data/2.5/forecast?q='.$city_name.'&appid=32ba0bfed592484379e51106cef3f204');
+        try
+        {
+            $weather = file_get_contents('https://api.openweathermap.org/data/2.5/forecast?q='.$this->cityName.'&appid=32ba0bfed592484379e51106cef3f204');
+        }
+        catch (\Exception $e)
+        {
+            $weather = FALSE;
+        }
+     
         return $weather;
     }
     
     public function weather()
     {
-        $weather = $this->get_forecast_by_location($this->get_curent_location($this->get_ip_addreess()));
+        $weather = $this->get_forecast_by_location();
+
+        if($weather) 
+        {
         $result = json_decode($weather);
 
-        $data = ["ip"=> $this->get_ip_addreess(),
-                "cityName" => $this->get_curent_location($this->get_ip_addreess()),
+        $data = ["ip"=> $this->ip,
+                "cityName" => $this->cityName,
                 "days"=>$result->list
                 ];
+        } else {
+            $data = ["ip"=> $this->ip,
+                    "cityName" => $this->cityName,
+                    "days"=>[]
+                    ];
+
+        }
 
         return view('weather::weather',$data);
     }
@@ -75,17 +108,18 @@ class ForecastController extends Controller
 
         $forecast = Forecast::create($request_data);
 
-        $weather = $this->get_forecast_by_location($this->get_curent_location($request_data['ip']));
+        $weather = $this->get_forecast_by_location();
         $result = json_decode($weather);
 
         $data = ["ip"=> $request_data['ip'],
-                "cityName" => $this->get_curent_location($request_data['ip']),
+                "cityName" => $this->cityName,
                 "days"=>$result->list
                 ];
 
         $days=[];
 
-        foreach($data['days'] as $key=>$value){
+        foreach($data['days'] as $key=>$value)
+        {
             $days[$key]['datetime'] = $value->dt_txt;
             $days[$key]['forecast_id'] = $forecast->id;
             $days[$key]['temp_min'] = $value->main->temp_min;
